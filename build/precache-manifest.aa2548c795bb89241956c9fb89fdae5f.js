f4585d1cd3e7e42ed4d8184e15eff79953b1bd5d
PCFilicetti <peter.filicetti@kingsmantech.com>
2018-10-19 09:45:03 -0400

    let's try this

---
+++ build/precache-manifest.aa2548c795bb89241956c9fb89fdae5f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d8c80e274b165a660524",
+    "url": "/static/js/main.d8c80e27.chunk.js"
+  },
+  {
+    "revision": "a75c3ad7ae634336baaa",
+    "url": "/static/js/1.a75c3ad7.chunk.js"
+  },
+  {
+    "revision": "d8c80e274b165a660524",
+    "url": "/static/css/main.9a7e2196.chunk.css"
+  },
+  {
+    "revision": "b2e33c223a70da1454d2835536a4ba1f",
+    "url": "/index.html"
+  }
+];
